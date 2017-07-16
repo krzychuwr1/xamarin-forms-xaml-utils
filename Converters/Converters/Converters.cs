8f202bc24a8ac56055c26d7e469050a1f121ce0c
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-07-16 13:34:36 +0200

    Added converters project and IsNotNull converter

---
+++ Converters/Converters/Converters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Converters
+{
+    public class IsNotNull : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value != null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
